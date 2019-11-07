c2d9f257465c87a1080bb69808b7f620f23bcf4c
artofnext <novnovalex@gmail.com>
2019-11-07 14:49:34 +0200

    Init commit

---
+++ js/shopping_list.js
@@ -0,0 +1,27 @@
+let sh_list = [
+    {
+        name: "Meat",
+        amount: 2,
+        isBought: false,
+    },
+    {
+        name: "Pasta",
+        amount: 1,
+        isBought: false,
+    },
+    {
+        name: "Butter",
+        amount: 8,
+        isBought: false,
+    },
+    {
+        name: "Honey",
+        amount: 4,
+        isBought: false,
+    },
+    {
+        name: "Bread",
+        amount: 5,
+        isBought: true,
+    },
+]
